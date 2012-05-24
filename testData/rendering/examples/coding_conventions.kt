7c44b3b7c202684f7b1f52e30c7d39ea5f521b64
Natalia.Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2012-05-24 13:57:13 +0400

    Add new tests for multiline tokens

---
+++ testData/rendering/examples/coding_conventions.kt
@@ -0,0 +1,12 @@
+/**
+ * This is **bold**
+ *
+ * New paragraph using newlines.
+ *
+ * We can also support wiki links notation
+ * for example [[List]] and [[Collection]] and [[java.util.Collection.size()]] will
+ * generate hypertext links to classes
+ *
+ * @includeFunctionBody ../test/MyTest.kt someFunction
+ */
+fun anotherMethod(): Unit { ... }
---
+++ testData/rendering/examples/multilineString.kt
@@ -0,0 +1,5 @@
+fun foo() {
+  val multilineStr = """First line
+                        Second line
+                        Third line"""
+}
